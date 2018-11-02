fa95853fd301694c5518ed11886ba51de9da73b0
feigege <312012733@qq.com>
2018-11-02 16:17:57 +0800

    -add

---
+++ bin/kafka-consumer-service - 8002.bat
@@ -0,0 +1,7 @@
+@echo off
+title kafka-consumer-service - 8002
+
+cd f:
+f:
+cd temp
+java -jar kafka-consumer-service-0.0.1-SNAPSHOT.jar --server.port=8002
---
+++ bin/kafka-producer-service - 8001.bat
@@ -0,0 +1,7 @@
+@echo off
+title kafka-producer-service - 8001
+
+cd f:
+f:
+cd temp
+java -jar kafka-producer-service-0.0.1-SNAPSHOT.jar --server.port=8001
