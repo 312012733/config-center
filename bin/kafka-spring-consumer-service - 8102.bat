e185db9ad19bdcea9388538bda666d9256c00a2b
feigege <312012733@qq.com>
2018-11-05 15:37:33 +0800

    d

---
+++ bin/kafka-spring-consumer-service - 8102.bat
@@ -0,0 +1,7 @@
+@echo off
+title kafka-spring-consumer-service - 8102
+
+cd f:
+f:
+cd temp
+java -jar kafka-spring-consumer-service-0.0.1-SNAPSHOT.jar --server.port=8102
---
+++ bin/kafka-spring-producer-service - 8101.bat
@@ -0,0 +1,7 @@
+@echo off
+title kafka-spring-producer-service - 8101
+
+cd f:
+f:
+cd temp
+java -jar kafka-spring-producer-service-0.0.1-SNAPSHOT.jar --server.port=8101
